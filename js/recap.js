cf22d838f5e3f0c3107c723a116580670109a499
Hutch Vivian <hutch.vivian@gmail.com>
2014-04-09 01:01:45 +0000

    push and pray the new js works

--- js/recap.js
+++ js/recap.js
@@ -1,5 +1,23 @@
 $(function() {
+	$(".matchup").each(function(i, el) {
+		$el = $(el);
+		var home = $el.find(".home").text;
+		var away = $el.find(".away").text;
+		var date = $(".jumbotron").find("h2").text.split("/");
+		var year = date[2];
+		var month = date[1];
+		var day = date[0].slice(9);
+		var postgame = $el.find(".postgame");
 
-	$(".
-
+		$el.find(".btn").click(function() {
+			$.get("/generate", {"home": home,
+									  "away": away, 
+									  "year": year,
+									  "month": month,
+									  "day": day
+									 }, function(data) {
+				postgame.val(data);
+			}); 
+		}
+	});
 });
